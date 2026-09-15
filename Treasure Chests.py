cb0415dfce9bf3cdb5c986643999d6bcb58618e9
Korkor Ndede Kojo <k.ndedekojo@alustudent.com>
2026-09-16 00:04:47 +0400

    Added the Treasure Chests file

--- Treasure Chests.py
+++ Treasure Chests.py
@@ -1,7 +1,14 @@
-#This function opens chests using a for loop. Each chest gives 10 gold.
+# This function opens chests using a for loop.
+# Each chest gives 10 gold.
 def collect_gold(number_of_chests):
+    gold = 0
+
     for chest in range(number_of_chests):
         print("You found 10 gold!")
+        gold = gold + 10
+
+    print("You collected", gold, "gold!")
+
 
 # This asks how many chests to open.
 chests = int(input("How many chests do you want to open? "))
